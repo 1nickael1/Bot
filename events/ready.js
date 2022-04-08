d92fb4fc602444f93755cfb7cd13ba8abbb5d6db
Nickael Vereza Bruzzi <qaznickael@protonmail.com>
2022-04-08 09:17:27 -0300

    test deploy

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
   once: true,
   execute(client) {
     console.log('\033[0;33m' + `${client.user.tag}` + '  \033[0;36miniciado!\033[0;0m');
-    client.user.setPresence({ activities: [{ name: "discord.gg/pdl" }] });
+    client.user.setPresence({ activities: [{ name: "eu mesmo" }] });
   },
 };
