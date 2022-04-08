af70cbab1b96ede8e84303f8dbae8a6dc36be4e1
Nickael Vereza Bruzzi <qaznickael@protonmail.com>
2022-04-08 13:02:39 -0300

    test commands

--- commands/testes/meuteste.js
+++ commands/testes/meuteste.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "meuteste",
     permissions: ["SEND_MESSAGES"],
     run: async (client, message, args) => {
-      return message.channel.send({content: "Funcionou!"});
+      return message.channel.send({content: "meu teste!"});
     },
 };
